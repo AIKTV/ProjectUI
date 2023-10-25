a3e33d0a77fc68425f19fb8963a8d8f5e95e4b3d
gongfuture <gongfuture@outlook.com>
2023-10-25 21:26:24 +0800

    file open temple

--- CallMainWindow.py
+++ CallMainWindow.py
@@ -1,16 +1,19 @@
 #!/user/bin/env python
 # -*- coding:utf-8 -*-
 # Code created by gongfuture
-# Create Time: 2023/10/25 
+# Create Time: 2023/10/25
 # Create User: gongf
 # This file is a part of ProjectUI
 #
 
+import warnings
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets
 from fileopen import Ui_MainWindow
 import sys
 import os
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class MainForm(QMainWindow, Ui_MainWindow):
@@ -20,10 +23,11 @@ class MainForm(QMainWindow, Ui_MainWindow):
         self.actionfileopen.triggered.connect(self.open_file)
 
     def open_file(self):
-        fileName, fileType = QtWidgets.QFileDialog.getOpenFileName(self, "选取文件", os.getcwd(),
-                                                                   "All Files(*);;Text Files(*.txt)")
+        fileName, fileType = QtWidgets.QFileDialog.getOpenFileName(
+            self, "选取文件", os.getcwd(), 'All Files(*);;Text Files(*.txt)')
         print(fileName)
         print(fileType)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
