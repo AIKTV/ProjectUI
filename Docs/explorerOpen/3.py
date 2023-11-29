8a5956dc066205770906b49333871745b93d9611
Pan-xyz <3590338435@qq.com>
2023-11-29 19:01:05 +0800

    将导出文件（音频）放入已经指定了的目录（在代码中指定）然后在UI界面设计一个按钮 点击打开指定文件夹选择播放导出文件（音频）

--- Docs/explorerOpen/3.py
+++ Docs/explorerOpen/3.py
@@ -1,35 +1,31 @@
-#将导出文件（音频）放入已经指定了的目录（在代码中指定）然后在UI界面设计一个小按钮 点击就立即播放指定文件夹中的导出文件（音频）
-ifrom PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtMultimedia import QMediaPlayer
-from generated_ui import Ui_MainWindow
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QDir
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
+from mainwindow import Ui_MainWindow  # 导入从UI文件生成的Python类
 
-        # 加载UI界面
+
+class AudioPlayer(QMainWindow):
+    def __init__(self, folder_path):
+        super(AudioPlayer, self).__init__()
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.ui.setupUi(self)  # 初始化UI界面
+        self.ui.pushButton.clicked.connect(self.open_folder)  # 点击按钮连接到open_folder函数
+        self.folder_path = folder_path
 
-        # 连接按钮的信号槽
-        self.ui.playButton.clicked.connect(self.playAudio)
+    def open_folder(self):
+        folder_path = QDir.toNativeSeparators(self.folder_path)  # 转换为本地平台的路径格式
+        os.startfile(folder_path)  # 打开指定路径的文件夹
 
-    def playAudio(self):
-        # 指定导出文件夹路径
-        export_folder = "指定的文件夹路径"
+    def run(self):
+        app = QApplication(sys.argv)  # 创建Qt应用程序
+        self.show()  # 显示主窗口
+        sys.exit(app.exec_())  # 运行应用程序，并进入事件循环
 
-        # 创建MediaPlayer对象
-        player = QMediaPlayer()
-
-        # 设置音频文件路径
-        audio_file = f"{export_folder}/导出文件名.wav"  # 根据实际情况修改文件名和格式
-        player.setMedia(QMediaContent.fromUrl(QUrl.fromLocalFile(audio_file)))
-
-        # 播放音频
-        player.play()
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec_()
+    folder_path = "指定的目录路径"  # 在这里替换为你自己指定的目录路径
+    player = AudioPlayer(folder_path)
+    player.run()
+
