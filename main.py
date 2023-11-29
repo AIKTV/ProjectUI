9532b56b0582d4063a4c764450cfc9b423f17abd
gongfuture <gongfuture@outlook.com>
2023-11-29 18:41:55 +0800

    界面可以显示，接下来是各部分控件与代码链接

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from AIKTVUI import Ui_MainWindow
 import sys
 import os
 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 class MainForm(QMainWindow, Ui_MainWindow):
-    class MainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainForm, self).__init__()
+        self.setupUi(self)
 
 
 
-
-        if __name__ == "__main__":
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     win = MainForm()
     win.show()
