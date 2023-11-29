d7d9e30c8056328b4cf8b14ac7ed67b90ce06f11
fAx1A <qinyang0406@gmail.com>
2023-11-29 21:18:54 +0800

    AIKTVUI

--- AIKTVUI.py
+++ AIKTVUI.py
@@ -10,38 +10,40 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow,QVBoxLayout,QLabel,QLineEdit,QPushButton,QInputDialog,QMessageBox,QDialog
 from PyQt5.QtCore import Qt,QProcess
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore,QtGui,QtWidgets
+
+if_enhance = 'n'
 
 
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
+    def setupUi(self,MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setEnabled(True)
-        MainWindow.resize(932, 674)
-        MainWindow.setMinimumSize(QtCore.QSize(800, 450))
-        MainWindow.setMaximumSize(QtCore.QSize(1600, 900))
+        MainWindow.resize(932,674)
+        MainWindow.setMinimumSize(QtCore.QSize(800,450))
+        MainWindow.setMaximumSize(QtCore.QSize(1600,900))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.configWidget = QtWidgets.QWidget(self.centralwidget)
-        self.configWidget.setMinimumSize(QtCore.QSize(191, 181))
-        self.configWidget.setMaximumSize(QtCore.QSize(401, 221))
+        self.configWidget.setMinimumSize(QtCore.QSize(191,181))
+        self.configWidget.setMaximumSize(QtCore.QSize(401,221))
         self.configWidget.setObjectName("configWidget")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.configWidget)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.chooseRecordGroupBox = QtWidgets.QGroupBox(self.configWidget)
-        self.chooseRecordGroupBox.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.chooseRecordGroupBox.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
         self.chooseRecordGroupBox.setObjectName("chooseRecordGroupBox")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.chooseRecordGroupBox)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.chooseRecord = QtWidgets.QPushButton(self.chooseRecordGroupBox)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.chooseRecord.sizePolicy().hasHeightForWidth())
         self.chooseRecord.setSizePolicy(sizePolicy)
-        self.chooseRecord.setMaximumSize(QtCore.QSize(75, 23))
+        self.chooseRecord.setMaximumSize(QtCore.QSize(75,23))
         self.chooseRecord.setObjectName("chooseRecord")
         self.horizontalLayout_2.addWidget(self.chooseRecord)
         self.recordAddress = QtWidgets.QLineEdit(self.chooseRecordGroupBox)
@@ -49,17 +51,17 @@ class Ui_MainWindow(object):
         self.horizontalLayout_2.addWidget(self.recordAddress)
         self.verticalLayout_2.addWidget(self.chooseRecordGroupBox)
         self.chooseHandledGroupBox = QtWidgets.QGroupBox(self.configWidget)
-        self.chooseHandledGroupBox.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.chooseHandledGroupBox.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
         self.chooseHandledGroupBox.setObjectName("chooseHandledGroupBox")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.chooseHandledGroupBox)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.chooseHandled = QtWidgets.QPushButton(self.chooseHandledGroupBox)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.chooseHandled.sizePolicy().hasHeightForWidth())
         self.chooseHandled.setSizePolicy(sizePolicy)
-        self.chooseHandled.setMaximumSize(QtCore.QSize(75, 23))
+        self.chooseHandled.setMaximumSize(QtCore.QSize(75,23))
         self.chooseHandled.setObjectName("chooseHandled")
         self.horizontalLayout.addWidget(self.chooseHandled)
         self.handledAddress = QtWidgets.QLineEdit(self.chooseHandledGroupBox)
@@ -74,7 +76,7 @@ class Ui_MainWindow(object):
         self.configButton = QtWidgets.QPushButton(self.configWidget)
         self.configButton.setObjectName("configButton")
         self.verticalLayout_2.addWidget(self.configButton)
-        self.gridLayout_2.addWidget(self.configWidget, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.configWidget,0,0,1,1)
         self.recordWidget = QtWidgets.QWidget(self.centralwidget)
         self.recordWidget.setObjectName("recordWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.recordWidget)
@@ -97,7 +99,7 @@ class Ui_MainWindow(object):
         self.recordOutputDisplay.setText("")
         self.recordOutputDisplay.setObjectName("recordOutputDisplay")
         self.verticalLayout.addWidget(self.recordOutputDisplay)
-        self.gridLayout_2.addWidget(self.recordWidget, 0, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.recordWidget,0,1,1,1)
         self.playWidget = QtWidgets.QWidget(self.centralwidget)
         self.playWidget.setObjectName("playWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.playWidget)
@@ -113,7 +115,7 @@ class Ui_MainWindow(object):
         self.recordStopButton = QtWidgets.QPushButton(self.recordPlayBox)
         self.recordStopButton.setObjectName("recordStopButton")
         self.horizontalLayout_4.addWidget(self.recordStopButton)
-        self.gridLayout.addWidget(self.recordPlayBox, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.recordPlayBox,0,0,1,1)
         self.handledPlayBox = QtWidgets.QGroupBox(self.playWidget)
         self.handledPlayBox.setAlignment(QtCore.Qt.AlignCenter)
         self.handledPlayBox.setObjectName("handledPlayBox")
@@ -125,52 +127,50 @@ class Ui_MainWindow(object):
         self.handledStopButton = QtWidgets.QPushButton(self.handledPlayBox)
         self.handledStopButton.setObjectName("handledStopButton")
         self.horizontalLayout_5.addWidget(self.handledStopButton)
-        self.gridLayout.addWidget(self.handledPlayBox, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.handledPlayBox,0,1,1,1)
         self.playFrame = QtWidgets.QFrame(self.playWidget)
         self.playFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.playFrame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.playFrame.setObjectName("playFrame")
         self.time_lbl = QtWidgets.QLabel(self.playFrame)
-        self.time_lbl.setGeometry(QtCore.QRect(390, 110, 72, 15))
+        self.time_lbl.setGeometry(QtCore.QRect(390,110,72,15))
         self.time_lbl.setObjectName("time_lbl")
         self.time_lbl_2 = QtWidgets.QLabel(self.playFrame)
-        self.time_lbl_2.setGeometry(QtCore.QRect(480, 110, 72, 15))
+        self.time_lbl_2.setGeometry(QtCore.QRect(480,110,72,15))
         self.time_lbl_2.setObjectName("time_lbl_2")
         self.time_slider = QtWidgets.QSlider(self.playFrame)
-        self.time_slider.setGeometry(QtCore.QRect(170, 70, 571, 22))
+        self.time_slider.setGeometry(QtCore.QRect(170,70,571,22))
         self.time_slider.setOrientation(QtCore.Qt.Horizontal)
         self.time_slider.setObjectName("time_slider")
         self.play_btn = QtWidgets.QPushButton(self.playFrame)
-        self.play_btn.setGeometry(QtCore.QRect(50, 70, 93, 28))
+        self.play_btn.setGeometry(QtCore.QRect(50,70,93,28))
         self.play_btn.setObjectName("play_btn")
         self.mode_btn = QtWidgets.QPushButton(self.playFrame)
-        self.mode_btn.setGeometry(QtCore.QRect(770, 70, 93, 28))
+        self.mode_btn.setGeometry(QtCore.QRect(770,70,93,28))
         self.mode_btn.setObjectName("mode_btn")
-        self.gridLayout.addWidget(self.playFrame, 1, 0, 1, 2)
-        self.gridLayout_2.addWidget(self.playWidget, 1, 0, 1, 2)
+        self.gridLayout.addWidget(self.playFrame,1,0,1,2)
+        self.gridLayout_2.addWidget(self.playWidget,1,0,1,2)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self,MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.chooseRecordGroupBox.setTitle(_translate("MainWindow", "选择录音/原始音频文件"))
-        self.chooseRecord.setText(_translate("MainWindow", "选择文件"))
-        self.chooseHandledGroupBox.setTitle(_translate("MainWindow", "选择处理后音频文件"))
-        self.chooseHandled.setText(_translate("MainWindow", "选择文件"))
-        self.configButton.setText(_translate("MainWindow", "AI模型配置"))
-        self.recordTimeDisplay.setText(_translate("MainWindow", "录音时长：你还没有开始录音哦"))
-        self.recordButton.setText(_translate("MainWindow", "开始"))
-        self.pauseRecordButton.setText(_translate("MainWindow", "暂停"))
+        MainWindow.setWindowTitle(_translate("MainWindow","MainWindow"))
+        self.chooseRecordGroupBox.setTitle(_translate("MainWindow","选择录音/原始音频文件"))
+        self.chooseRecord.setText(_translate("MainWindow","选择文件"))
+        self.chooseHandledGroupBox.setTitle(_translate("MainWindow","选择处理后音频文件"))
+        self.chooseHandled.setText(_translate("MainWindow","选择文件"))
+        self.configButton.setText(_translate("MainWindow","AI模型配置"))
+        self.recordTimeDisplay.setText(_translate("MainWindow","录音时长：你还没有开始录音哦"))
+        self.recordButton.setText(_translate("MainWindow","开始"))
+        self.pauseRecordButton.setText(_translate("MainWindow","暂停"))
 
     def openConfigDialog(self):
         dialog = QtWidgets.QDialog(MainWindow)
         dialog.setWindowTitle("AI模型配置")
         layout = QtWidgets.QVBoxLayout(dialog)
-        label = QtWidgets.QLabel("AI模型配置")
-        layout.addWidget(label)                        
 
         self.model_name_label = QLabel("请输入使用的模型步数（例：模型为G_800.pth就输入800）")
         self.model_name_edit = QLineEdit()
@@ -232,58 +232,60 @@ class Ui_MainWindow(object):
         layout.addWidget(self.if_linear_gradient_label)
         layout.addWidget(self.if_linear_gradient_edit)
 
-        def start_conversion(self):
-            model_name = self.model_name_edit.text()
-            wav_name = self.wav_name_edit.text()
-            key_num = self.key_num_edit.text()
-            f0_predictor = self.f0_predictor_edit.text()
-            if f0_predictor == '0':
-                f0_predictor = 'crepe'
-            if f0_predictor == '1':
-                f0_predictor = 'pm'
-            if f0_predictor == '2':
-                f0_predictor = 'dio'
-            if f0_predictor == '3':
-                f0_predictor = 'harvest'
-            if_diffusion = self.if_diffusion_edit.text()
-            diffusion_name = self.diffusion_name_edit.text()
-            diffusion_k_step = self.diffusion_k_step_edit.text()
-
-            if_cluster = self.if_cluster_edit.text()
-            cluster_ratio = self.cluster_ratio_edit.text()
-
-            if_auto_predict_f0 = self.if_auto_predict_f0_edit.text()
-
-            if_clip = self.if_clip_edit.text()
-            if_linear_gradient = self.if_linear_gradient_edit.text()
-
-            inference_case = r'.\env\python.exe inference_main.py'
-            inference_case = inference_case + f' -m "logs/44k/G_{model_name}.pth" -c "configs/config.json" -n "{wav_name}" -t {key_num} -s "barbara" -f0p {f0_predictor}'
-
-            if if_diffusion == 'y':
-                inference_case = inference_case + f' -shd -dm "logs/44k/diffusion/model_{diffusion_name}.pt" -ks {diffusion_k_step}'
-            if if_cluster == 'y':
-                inference_case = inference_case + f' -cm "logs/44k/kmeans_10000.pt" -cr {cluster_ratio}'
-            if if_auto_predict_f0 == 'y':
-                inference_case = inference_case + f' -a'
-
-            inference_case = inference_case + f' -cl {if_clip} -lg {if_linear_gradient}'
-
-            process = QProcess()
-            process.start(inference_case)
-            process.waitForFinished()
-            process.close()
-            process.deleteLater()
-
-            QMessageBox.information(self,"转换完成","转换完成！")
-
         start_button = QtWidgets.QPushButton("开始转换")
-        start_button.clicked.connect(start_conversion)
+        start_button.clicked.connect(self.start_conversion)
         layout.addWidget(start_button)
         dialog.exec_()
 
+    def start_conversion(self):
+        model_name = self.model_name_edit.text()
+        wav_name = self.wav_name_edit.text()
+        key_num = self.key_num_edit.text()
+        f0_predictor = self.f0_predictor_edit.text()
+        if f0_predictor == '0':
+            f0_predictor = 'crepe'
+        if f0_predictor == '1':
+            f0_predictor = 'pm'
+        if f0_predictor == '2':
+            f0_predictor = 'dio'
+        if f0_predictor == '3':
+            f0_predictor = 'harvest'
+        if_diffusion = self.if_diffusion_edit.text()
+        diffusion_name = self.diffusion_name_edit.text()
+        diffusion_k_step = self.diffusion_k_step_edit.text()
+
+        if_cluster = self.if_cluster_edit.text()
+        cluster_ratio = self.cluster_ratio_edit.text()
+
+        if_auto_predict_f0 = self.if_auto_predict_f0_edit.text()
+
+        if_clip = self.if_clip_edit.text()
+        if_linear_gradient = self.if_linear_gradient_edit.text()
+
+        inference_case = r'.\env\python.exe inference_main.py'
+        inference_case = inference_case + f' -m "logs/44k/G_{model_name}.pth" -c "configs/config.json" -n "{wav_name}" -t {key_num} -s "barbara" -f0p {f0_predictor}'
+
+        if if_diffusion == 'y':
+            inference_case = inference_case + f' -shd -dm "logs/44k/diffusion/model_{diffusion_name}.pt" -ks {diffusion_k_step}'
+        if if_cluster == 'y':
+            inference_case = inference_case + f' -cm "logs/44k/kmeans_10000.pt" -cr {cluster_ratio}'
+        if if_auto_predict_f0 == 'y':
+            inference_case = inference_case + f' -a'
+
+        inference_case = inference_case + f' -cl {if_clip} -lg {if_linear_gradient}'
+
+        process = QProcess()
+        process.start(inference_case)
+        process.waitForFinished()
+        process.close()
+        process.deleteLater()
+
+        QtWidgets.QMessageBox.information(MainWindow,"转换完成","转换已完成！")
+
+
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
